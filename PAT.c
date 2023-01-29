7bfd2b9eadc357098ddd4409340fe82ac8c5c8a0
KDG <kingdalfgoodman@foxmail.com>
2023-01-29 16:18:53 +0800

    test remark

---
+++ PAT.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+int main()
+{
+    printf("HOLLE WORLD!\n");
+    system("pause");
+    printf("开始\n");
+
+    int a[4]={0,2,3,4};
+    for(int i=0;i<4;i++)
+        printf("%d\n",a[i]);
+        system("pause");
+
+    return 0;
+}
